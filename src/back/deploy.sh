0b99f7ed055c7ac4d6384fc6c12bad670fbfac47
liangzn <1763466626@qq.com>
2023-10-17 21:48:11 +0800

    feat: 更新脚本

--- src/back/deploy.sh
+++ src/back/deploy.sh
@@ -6,20 +6,18 @@ echo '进入脚本'
 # 进入到脚本所在的目录
 cd "$(dirname "$0")"
 
-pwd
-
 cd ../../ || exit
+
+pwd
 
 git pull --force
 
 yarn
 
-# cd ./src || exit
-
 echo '重新启动后端服务'
 
 # 重新启动项目
-pm2 restart ./src/app.js --name="back"
+pm2 restart ./src/app.js
 
 echo '后端服务更新成功'
 
--- src/front/deploy.sh
+++ src/front/deploy.sh
@@ -5,10 +5,10 @@ echo '进入脚本'
 # 进入到脚本所在的目录
 cd "$(dirname "$0")"
 
-pwd
-
 # 进入项目文件
 cd ../../../blog-vuepress || exit
+
+pwd
 
 # 拉取代码
 git pull --force
@@ -24,6 +24,6 @@ cd "$(dirname "$0")"
 
 echo '重新启动前端服务'
 
-pm2 restart server.js --name="front"
+pm2 restart server.js
 
 echo '前端服务更新成功'
