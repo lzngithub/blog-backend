fdab3b0fd7708cf88c2902e5cac65d74b14e905d
lzngithub <1763466626@qq.com>
2023-10-17 17:15:46 +0800

    feat: 调整脚本

--- src/back/deploy.sh
+++ src/back/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # 进入项目文件
-cd ../../../blog-backend || exit
+# cd ../../../blog-backend || exit
 
 git pull --force
 
 yarn
 
-cd ./src || exit
+# cd ./src || exit
 
 echo '重新启动后端服务'
 
 # 重新启动项目
-pm2 restart app.js
+pm2 restart ./src/app.js
 
 echo '后端服务更新成功'
 
