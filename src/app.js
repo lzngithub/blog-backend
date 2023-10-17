39c6f2539dc766c5709dca95e594c964765c3994
liangzn <1763466626@qq.com>
2023-10-17 22:23:23 +0800

    feat: 测试webhooks

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-var http = require('http');
-var handler = require('./front/index.js');
-var handlerBack = require('./back/index.js');
+var http = require("http");
+var handler = require("./front/index.js");
+var handlerBack = require("./back/index.js");
 
 http
   .createServer(function (req, res) {
     handler(req, res, function (err) {
       res.statusCode = 404;
-      res.end('no such location');
-      console.log('匹配不到路由');
+      res.end("no such location");
+      console.log("匹配不到路由");
     });
     handlerBack(req, res, function (err) {
       res.statusCode = 404;
-      res.end('no such location');
-      console.log('匹配不到路由');
+      res.end("no such location");
+      console.log("匹配不到路由");
     });
   })
   .listen(3000, () => {
-    console.log(new Date() + 'WebHook启动运行端口：', 3000);
+    console.log(new Date() + "：WebHook启动运行端口：", 3000);
   });
