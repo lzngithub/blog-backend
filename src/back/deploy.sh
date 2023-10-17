47f5e6b52d7c1c06f27846796309286d046dac3b
lzngithub <1763466626@qq.com>
2023-10-17 17:50:33 +0800

    fix: 修改部署脚本

--- src/back/deploy.sh
+++ src/back/deploy.sh
@@ -2,8 +2,13 @@
 
 # 进入项目文件
 echo '进入脚本'
+
+# 进入到脚本所在的目录
+cd "$(dirname "$0")"
+
 pwd
-# cd ../../../blog-backend || exit
+
+cd ../../ || exit
 
 git pull --force
 
@@ -14,7 +19,7 @@ yarn
 echo '重新启动后端服务'
 
 # 重新启动项目
-pm2 restart ./src/app.js
+pm2 restart ./src/app.js --name="back"
 
 echo '后端服务更新成功'
 
