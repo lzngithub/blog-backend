137cffdfd051682c7da66341f272c5c6e7f5ba25
lzngithub <1763466626@qq.com>
2023-10-18 10:50:08 +0800

    feat: 整理代码

--- src/app.js
+++ src/app.js
@@ -8,14 +8,11 @@ http
       res.statusCode = 404;
       res.end('no such location');
       console.log(err);
-      console.log('匹配不到路由咯');
-      console.log('测试文本');
     });
     handlerBack(req, res, function (err) {
       res.statusCode = 404;
       res.end('no such location');
       console.log(err);
-      console.log('匹配不到路由了,测试');
     });
   })
   .listen(3000, () => {
