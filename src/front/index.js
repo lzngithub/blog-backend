f6e08f7fb323e3f2b9e149e371da6ed2716245d6
lzngithub <1763466626@qq.com>
2023-10-18 10:05:07 +0800

    feat: 文件路径调整

--- src/front/index.js
+++ src/front/index.js
@@ -1,32 +1,36 @@
-const createHandler = require("github-webhook-handler");
-const spawn = require("child_process").spawn;
-const handler = createHandler({ path: "/pushCode", secret: "liangzn" });
+const createHandler = require('github-webhook-handler');
+const spawn = require('child_process').spawn;
+const path = require('path');
+
+// 获取当前文件的绝对路径
+const filepath = path.join(__dirname, './deploy.sh');
+const handler = createHandler({ path: '/pushCode', secret: 'liangzn' });
 
 //监听发生错误
-handler.on("error", function (err) {
-  console.error("Error:", err);
+handler.on('error', function (err) {
+  console.error('Error:', err);
 });
 
 //监听push钩子 时触发函数
-handler.on("push", function (event) {
-  console.log("front");
-  console.log("收到一条更新来自：");
-  console.log("仓库" + event.payload.repository.name);
-  console.log("分支" + event.payload.ref);
+handler.on('push', function (event) {
+  console.log('front');
+  console.log('收到一条更新来自：');
+  console.log('仓库' + event.payload.repository.name);
+  console.log('分支' + event.payload.ref);
   const rumCommand = (cmd, args, callback) => {
     const child = spawn(cmd, args);
-    let response = "";
-    child.stdout.on("data", (buffer) => (response += buffer.toString()));
-    child.stdout.on("end", () => callback(response));
+    let response = '';
+    child.stdout.on('data', (buffer) => (response += buffer.toString()));
+    child.stdout.on('end', () => callback(response));
   };
-  if (event && event.payload && event.payload.ref === "refs/heads/main") {
-    console.log("开始自动构建");
+  if (event && event.payload && event.payload.ref === 'refs/heads/main') {
+    console.log('开始自动构建');
     // 执行自动部署脚本
-    rumCommand("sh", ["./deploy.sh"], (txt) => {
+    rumCommand('sh', [filepath], (txt) => {
       console.log(txt);
     });
   } else {
-    console.log("推送的不是main分支，不会触发自动构建");
+    console.log('推送的不是main分支，不会触发自动构建');
   }
 });
 
--- src/front/server.js
+++ src/front/server.js
@@ -1,7 +1,14 @@
 const express = require('express'); //npm下载并引入express模块 npm -express -D
+const path = require('path');
+
+// ./dist 为vue打包后dist文件夹的路径
+const filepath = path.join(
+  __dirname,
+  '../../../blog-vuepress/docs/.vuepress/dist'
+);
 
 const app = express();
-app.use(express.static('../../../blog-vuepress/docs/.vuepress/dist')); // ./dist 为vue打包后dist文件夹的路径
+app.use(express.static(filepath));
 app.listen(80, function (err) {
   //80 监听80端口号
   if (err) {
