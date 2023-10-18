ecce23812f3e84df49d1e11cf05c94300e783751
lzngithub <1763466626@qq.com>
2023-10-18 09:39:26 +0800

    fix: 测试提交

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-var http = require("http");
-var handler = require("./front/index.js");
-var handlerBack = require("./back/index.js");
+var http = require('http');
+var handler = require('./front/index.js');
+var handlerBack = require('./back/index.js');
 
 http
   .createServer(function (req, res) {
     handler(req, res, function (err) {
       res.statusCode = 404;
-      res.end("no such location");
+      res.end('no such location');
       console.log(err);
-      console.log("匹配不到路由咯");
-      console.log("测试文本");
+      console.log('匹配不到路由咯');
+      console.log('测试文本');
     });
     handlerBack(req, res, function (err) {
       res.statusCode = 404;
-      res.end("no such location");
+      res.end('no such location');
       console.log(err);
-      console.log("测试文本front");
-      console.log("匹配不到路由了,测试");
+      console.log('测试文本fronts');
+      console.log('匹配不到路由了,测试');
     });
   })
   .listen(3000, () => {
-    console.log(new Date() + "：WebHook启动运行端口：", 3000);
+    console.log(new Date() + '：WebHook启动运行端口：', 3000);
   });
